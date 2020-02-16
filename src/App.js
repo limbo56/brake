d10843f7c29f02da4e36ca997e77ef7c1ca8943e
David Rodriguez <15853933+limbo56@users.noreply.github.com>
2020-02-16 18:09:19 -0500

    Update bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { registerRootComponent } from 'expo'
+import { StyleSheet, Text, View, Platform } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { MaterialIcons } from '@expo/vector-icons';
 import HomeScreen from "~/screens/Home"
 import Calendar from "~/screens/Calendar"
 
@@ -12,14 +13,29 @@ const routes = [
   {
     name: 'Home',
     component: HomeScreen,
-    icon: 'ios-home'
+    icon: 'home'
   },
   {
     name: 'Calendar',
     component: Calendar,
-    icon: 'ios-calendar'
+    icon: 'today'
   }
 ]
+
+const iconSize = 28
+const styles = StyleSheet.create({
+  barStyle: {
+    backgroundColor: "#FFFFFF",
+    height: Platform.OS === "android" ? 60 : 100
+  },
+  labelStyle: {
+    fontSize: 14,
+  },
+  tabStyle: {
+    paddingTop: 10,
+    paddingBottom: Platform.OS === "android" ? 5 : 0
+  }
+})
 
 const App = () => (
   <SafeAreaProvider>
@@ -27,16 +43,11 @@ const App = () => (
       <Tab.Navigator
         initialRouteName="Home"
         tabBarOptions={{
-          style: {
-            height: 100,
-            paddingTop: 10
-          },
-          labelStyle: {
-            fontSize: 16,
-            margin: 0,
-            padding: 0,
-          },
-          activeTintColor: '#2A7BF6',
+          activeTintColor: "#E89A55",
+          inactiveTintColor: "#5C5C5C",
+          style: styles.barStyle,
+          labelStyle: styles.labelStyle,
+          tabStyle: styles.tabStyle
         }}
       >
         {routes.map(route => (
@@ -46,7 +57,11 @@ const App = () => (
             component={route.component}
             options={{
               tabBarIcon: ({ color }) => (
-                <Ionicons name={route.icon} color={color} size={30} />
+                <MaterialIcons
+                  name={route.icon}
+                  size={iconSize}
+                  color={color}
+                />
               )
             }}
           />
