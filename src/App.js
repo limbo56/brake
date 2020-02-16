15899a2ba739db5bdc5dc30a1dbbbf31cd83821e
David Rodriguez <15853933+limbo56@users.noreply.github.com>
2020-02-16 18:28:38 -0500

    Refactor navigation code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import { registerRootComponent } from 'expo'
-import { StyleSheet, Text, View, Platform } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { MaterialIcons } from '@expo/vector-icons';
-import HomeScreen from "~/screens/Home"
-import Calendar from "~/screens/Calendar"
-
-const Tab = createBottomTabNavigator()
-const routes = [
-  {
-    name: 'Home',
-    component: HomeScreen,
-    icon: 'home'
-  },
-  {
-    name: 'Calendar',
-    component: Calendar,
-    icon: 'today'
-  }
-]
+import routes from "~/routes"
 
 const iconSize = 28
 const styles = StyleSheet.create({
@@ -37,33 +22,37 @@ const styles = StyleSheet.create({
   }
 })
 
+const Tab = createBottomTabNavigator()
+const tabBarOptions = {
+  activeTintColor: "#E89A55",
+  inactiveTintColor: "#5C5C5C",
+  style: styles.barStyle,
+  labelStyle: styles.labelStyle,
+  tabStyle: styles.tabStyle
+}
+const tabScreenOptions = {
+  tabBarIcon: ({ color }) => (
+    <MaterialIcons
+      name={route.icon}
+      size={iconSize}
+      color={color}
+    />
+  )
+}
+
 const App = () => (
   <SafeAreaProvider>
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Home"
-        tabBarOptions={{
-          activeTintColor: "#E89A55",
-          inactiveTintColor: "#5C5C5C",
-          style: styles.barStyle,
-          labelStyle: styles.labelStyle,
-          tabStyle: styles.tabStyle
-        }}
+        tabBarOptions={tabBarOptions}
       >
         {routes.map(route => (
           <Tab.Screen
             key={route.name}
             name={route.name}
             component={route.component}
-            options={{
-              tabBarIcon: ({ color }) => (
-                <MaterialIcons
-                  name={route.icon}
-                  size={iconSize}
-                  color={color}
-                />
-              )
-            }}
+            options={tabScreenOptions}
           />
         ))}
       </Tab.Navigator>
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import HomeScreen from "~/screens/Home"
+import Calendar from "~/screens/Calendar"
+
+export default [
+    {
+        name: 'Home',
+        component: HomeScreen,
+        icon: 'home'
+    },
+    {
+        name: 'Calendar',
+        component: Calendar,
+        icon: 'today'
+    }
+]
